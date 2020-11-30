af0440982423a1b18fbefa5317ad4a1e052f3421
Rafael Augusto de Souza Silva <rafaelaugusto335@gmail.com>
2020-11-30 19:55:32 -0300

    Codigo do thiago

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { ChartType } from 'chart.js';
 import { ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
 import {HttpClient }from '@angular/common/http';
+import {io} from 'socket.io-client';
 
 @Component({
   selector: 'app-tab1',
@@ -17,16 +18,28 @@ export class Tab1Page {
   public chartData1: ChartDataSets[] = [{data: [], label: 'Histórico de umidade'}];
   public chartType1: ChartType = 'line';
   public chartLabels1: Label[]
+  socket : any;
 
   constructor(private httpClient: HttpClient) {
 
+      this.socket = io('http://localhost:3000/')
       this.loadData();
-      this.loadData1();
     
     
   }
-  valorUmidade = 23.6; // valores que vão ser recebidos do serviço
-  valorTemperatura = 32.4;
+  valorUmidade = 0; // valores que vão ser recebidos do serviço
+  valorTemperatura = 0;
+
+  ngOnInit(){
+    this.socket.on('sensorData', (message) => {
+      this.valorTemperatura = message.temperature
+      this.valorUmidade = message.umidity
+    })
+    this.socket.emit('set-name','haha');
+  }
+
+
+  /*
   loadData(){
 
     const request: string = 'http://api.marketstack.com/v1/eod?access_key=5b8a5c0833a40aaa9d468e0f6ac97a19&symbols=AAPL&date_from=2020-05-05&date_to=2020-09-09'
@@ -45,22 +58,49 @@ export class Tab1Page {
         this.chartData[0].data.push(data[i].close);
       }
     })   
+  }*/
+  loadData(){
+    const request: string = 'http://127.0.0.1:3000/get/average/week/temperature'
+    //const request: string = 'http://api.marketstack.com/v1/eod?access_key=5b8a5c0833a40aaa9d468e0f6ac97a19&symbols=AAPL&date_from=2020-05-05&date_to=2020-09-09'
+    
+    //call first time 
+    this.callApi()
+
+
+    //call every # ######
+    setInterval(() => {
+      this.callApi()
+    }, 60000)
+    
   }
-  loadData1(){
-    const request: string = 'http://api.marketstack.com/v1/eod?access_key=5b8a5c0833a40aaa9d468e0f6ac97a19&symbols=AAPL&date_from=2020-05-05&date_to=2020-09-09'
+
+  callApi(){
+    const request: string = 'http://127.0.0.1:3000/get/average/week/temperature'
+
     this.httpClient.get(request).subscribe(res =>{
       console.log(res)
 
-      const data:any = (res as any).data;
+      const data:any = (res as any);
 
-      this.chartLabels1 = [];
-    this.chartData1[0].data = []; 
+      this.chartLabels = [...data.label];
+      this.chartLabels1 = [...data.label];
 
+      const averageUmidity = data.averages.map((item) => {
+        return item.umidity
+      })
+
+      const averageTemp = data.averages.map((item) => {
+        return item.temp
+      })
+    this.chartData1[0].data = [...averageUmidity]; 
+    this.chartData[0].data = [...averageTemp]; 
+    
+    /*
       for(let i = 0; i<data.length; i++){
-        const date: Date = new Date(data[i].date);
-        this.chartLabels1.push(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`);
+        //const date: Date = new Date(data[i].date);
+        //this.chartLabels1.push(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`);
         this.chartData1[0].data.push(data[i].close);
-      }
+      }*/
     })
   }
 }
